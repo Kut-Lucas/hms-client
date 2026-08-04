0f842f9eaf722caa880d1e346f182bae80ee36c2
Kut-Lucas <codingkut@gmail.com>
2026-08-04 05:07:12 +0300

    Allow Render hostname

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,28 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: true,
+//     port: 5173,
+//   },
+// });
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
-    host: true,
+    host: "0.0.0.0",
     port: 5173,
+    allowedHosts: ["hms-client-f8lm.onrender.com"],
+  },
+
+  preview: {
+    host: "0.0.0.0",
+    allowedHosts: ["hms-client-f8lm.onrender.com"],
   },
 });
